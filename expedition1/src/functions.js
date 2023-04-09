1043ee3766cf7f4a08f56e4c90b3ee1f31bca4b4
Sirius Black <mariia.beliaev@gmail.com>
2023-04-10 00:28:46 +0300

    Life on Mars by David Bowie starts playing

--- expedition1/src/functions.js
+++ expedition1/src/functions.js
@@ -1,35 +1,44 @@
+const { log } = require('console');
+const fs = require('fs');
 
+const PATH = './src';
 
-//позволяет выбрать самого опытного капитана
+const crewInfo = fs.readFileSync(PATH + '/crew.txt', 'utf-8').split('\r\n').slice(1).map((el) => el.split(', '));
+const roverInfo = fs.readFileSync('./src/equipment.txt', 'utf-8').split('\r\n').slice(1).map((el) => el.split(', '));
+const rocketInfo = fs.readFileSync('./src/rockets.txt', 'utf-8').split('\r\n').slice(1).map((el) => el.split(', '));
+
+// позволяет выбрать самого опытного капитана
 function getRightCaptain() {
-
+  const bestCaptain = crewInfo.filter(([, , position,]) => position === 'Капитан').sort((a, b) => b[3] - a[3]);
+  return bestCaptain[0].join(', ');
 }
 
-//позволяет выбрать самого опытного врача среди женщин
+// позволяет выбрать самого опытного врача среди женщин
 function getRightDoc() {
-
+  const bestFemaleDoctor = crewInfo.filter(([, sex, position,]) => sex === 'ж' && position === 'Врач').sort((a, b) => +b[3] - +a[3]);
+  return bestFemaleDoctor[0].join(', ');
 }
 
-//позволяет выбрать всех бортмехаников
+// позволяет выбрать всех бортмехаников
 function getAllEngineer() {
-
+  return crewInfo.filter(([, , position,]) => position === 'Бортмеханик').map((el) => el.join(', '));
 }
 
-//Позволяет отобрать все марсоходы
-function getAllRover(){
-
+// Позволяет отобрать все марсоходы
+function getAllRover() {
+  return roverInfo.filter(([, type, ]) => type === 'марсоход').map((el) => el.join(', '));
 }
 
-//позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
-function getRightRovers(){
-
+// позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
+function getRightRovers() {
+  return roverInfo.filter(([, type, age]) => type === 'марсоход' && age > 3).map((el) => el.join(', '));
 }
 
-//позволяет выбрать ракету с максимальной дальностью полёта
-function getRightRocket(){
-
+// позволяет выбрать ракету с максимальной дальностью полёта
+function getRightRocket() {
+  const bestRocket = rocketInfo.sort((a, b) => b[2] - a[2]);
+  return bestRocket[0].join(', ');
 }
-
 
 module.exports = {
   getRightCaptain,
@@ -37,5 +46,5 @@ module.exports = {
   getRightDoc,
   getAllRover,
   getRightRovers,
-  getRightRocket
+  getRightRocket,
 };
